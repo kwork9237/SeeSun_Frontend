b40dc05b855b60b439131e65fa2be842a57cb682
gwonminseong665 <gwonminseong665@gmail.com>
2026-01-19 17:58:02 +0900

    MyPage를 admin, mento, mentee로 분리해서 처리

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <Route path='/Login' element={<Login/>}/>
 
           {/* 마이페이지 */}
-          <Route path='/mypage' element={<MyPage/>}/>
+          <Route path='/Mypage' element={<MyPage/>}/>
 
           {/* 강의 목록 */}
           <Route path='/lecture' element={<LectureList/>}/>
---
+++ src/pages/mypage/Admin.jsx
@@ -0,0 +1,11 @@
+// 관리자 페이지
+
+const Admin = () => {
+    return (
+        <div>
+            admin
+        </div>
+    );
+}
+
+export default Admin;
